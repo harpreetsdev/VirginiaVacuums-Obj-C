b6673a7f24db662e1f5fc23a5d8faabc7b8fedfd
Singh, Harpreet <Harpreet.Singh2@capitalone.com>
2016-01-21 11:47:45 -0500

    Added Service factory singleton class as a Data model and ProductsData.json file to carry dummy data for JSON serialization.

---
+++ VirginiaVacuums-Obj-c/ServiceFactory.h
@@ -0,0 +1,13 @@
+//
+//  ServiceFactory.h
+//  VirginiaVacuums-Obj-c
+//
+//  Created by Singh, Harpreet on 1/21/16.
+//  Copyright © 2016 HARPREET SINGH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ServiceFactory : NSObject
+
+@end
---
+++ VirginiaVacuums-Obj-c/ServiceFactory.m
@@ -0,0 +1,13 @@
+//
+//  ServiceFactory.m
+//  VirginiaVacuums-Obj-c
+//
+//  Created by Singh, Harpreet on 1/21/16.
+//  Copyright © 2016 HARPREET SINGH. All rights reserved.
+//
+
+#import "ServiceFactory.h"
+
+@implementation ServiceFactory
+
+@end
