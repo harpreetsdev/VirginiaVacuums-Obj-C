23bb7608dcb302403a0663b9c368539c1b096217
HARPREET SINGH <harpreetsingh@testserver.fios-router.home>
2016-01-23 17:46:44 -0500

    Added required tableView delegate methods - numberOfRowsInSection: and cellForRowAtIndexPath:

--- VirginiaVacuums-Obj-c/ViewController.m
+++ VirginiaVacuums-Obj-c/ViewController.m
@@ -18,11 +18,43 @@
 @end
 
 @implementation ViewController
+{
+    ServiceFactory *serviceFactory;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    //ServiceFactory *serviceFactory = [ServiceFactory sharedInstance];
+    serviceFactory = [ServiceFactory sharedInstance];
+
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    id <NSFetchedResultsSectionInfo> menuScreenInfo = [[serviceFactory.menuFetchedResultsController sections] objectAtIndex:section];
+    return [menuScreenInfo numberOfObjects];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    self.productsTableView = tableView;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    if (cell == nil) {
+        
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"]; // Subclass of the Custom matrix cell class.
+        
+        cell.selectionStyle = UITableViewCellSelectionStyleGray;
+        
+    }
+    
+        ProductDetail *productDetail = [serviceFactory.menuFetchedResultsController objectAtIndexPath:indexPath]; // The Credit Fetched results controller returns an Array of Fetched objects from the credit card section that can be used to populate the rows of the section.
+    
+    
+        cell.textLabel.text = @"RandomText";
+    
+    return cell;
+
 }
 
 - (void)didReceiveMemoryWarning {
