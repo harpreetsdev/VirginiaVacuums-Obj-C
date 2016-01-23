8db76ddb6c550d5f69ce11123417a0d2ff838e47
HARPREET SINGH <harpreetsingh@testserver.fios-router.home>
2016-01-23 00:43:47 -0500

    Converted Service factory class to a Singleton, moved Core data initialization code there from the App Delegate.

--- VirginiaVacuums-Obj-c/ServiceFactory.h
+++ VirginiaVacuums-Obj-c/ServiceFactory.h
@@ -7,7 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import <CoreData/CoreData.h>
 @interface ServiceFactory : NSObject
+
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+
++ (id)sharedInstance;
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 @end
