e4c77abd2d7ebd8551633eac7522e14495b9b9ea
Harpreet Singh <harpreetsdevelopers@gmail.com>
2016-10-09 11:55:16 -0400

    Added Input View Controller file and storyboard outlets.

---
+++ VirginiaVacuums-Obj-c/InputViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InputViewController.swift
+//  VirginiaVacuums-Obj-c
+//
+//  Created by HARPREET SINGH on 10/9/16.
+//  Copyright © 2016 HARPREET SINGH. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: NSObject {
+
+}
